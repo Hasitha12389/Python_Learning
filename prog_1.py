1316de0e9d1b122bd6fbf379d97e02e33cdfcfa4
M.A.H.D. NAVARATHNE <140165356+Hasitha12389@users.noreply.github.com>
2025-02-16 22:28:45 +0530

    Add files via upload

---
+++ prog_1.py
@@ -0,0 +1,13 @@
+# Use this # symbol for the comments
+print("welcome world")
+print()
+print("welcome world")
+print()
+
+
+# video 06
+print("Hello \nHello \nHello \nHello \nHello")
+print("Hello" + "Kamal" + "Sunil")
+print("This" + " " + "with" + " " + "speces")
+
+# ended video 06
